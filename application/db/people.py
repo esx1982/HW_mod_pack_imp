914328a85f6638c7d06ee7b025fd5c524a71004b
ESX <evgeniy_h@list.ru>
2023-11-17 20:04:14 +1000

    initial commit

---
+++ application/db/people.py
@@ -0,0 +1,3 @@
+
+def get_employees(name):
+    print(f'добрый день, {name}, Вы приняты на работу!')
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+
+
+def calculate_salary(name, job_title):
+    print(f'добрый день, {name}, Ваша должность - {job_title}.')
+
+
